fe217f20d69d35ae71b045e31e443e9363daa303
Luke Palmer <lrpalmer@gmail.com>
2014-02-20 01:09:56 -0800

    Wow, it's actually a pretty good harmonizer now :-)

--- wavelayer.hs
+++ wavelayer.hs
@@ -9,6 +9,7 @@ import Control.Applicative
 import Data.List (tails, sort)
 import Control.Monad (guard, join)
 import Control.Arrow (second)
+import Data.Maybe (listToMaybe, catMaybes)
 
 
 nub :: (Ord a) => [a] -> [a]
@@ -58,12 +59,28 @@ inRange lo hi x = lo <= x && x <= hi
 
 type W = Integer
 
+-- chordRoot [5/4,3/2]
+-- 5/4 = m*r
+-- 3/2 = n*r
+--
+-- suppose r = a/b.  multiply by b
+--
+-- 5/4*b = m*a
+-- 3/2*b = n*a
+-- 5*b = 4*m*a
+-- 3*b = 2*n*a
+--
+--
+-- 5 = 4*m*r
+-- 3 = 2*n*r
+--
+-- 
 
 -- input is ascending
 chordRoot :: [Rational] -> WS.T W Rational
 chordRoot [] = empty
 chordRoot (r1:rs) = do
-    posn' <- wChoice [ pure n | n <- [1..] ]
+    posn' <- wChoice [ pure (posn % n)  | n <- [1..] ]
     return $ r1 / posn'
     where
     ratios = map (/r1) rs
@@ -72,9 +89,29 @@ chordRoot (r1:rs) = do
 harmonize :: [Rational] -> WS.T W Rational
 harmonize rs = do
     root <- chordRoot rs
-    harm <- wChoice [ pure (n*root) | n <- [1..] ]
+    harm <- wChoice [ pure (n*root) | n <- [1..] ] 
     guard (harm `notElem` rs)
     return $ harm
+
+-- The chord roots proceed as r1/(posn/n) = (r1/posn)*n.  
+-- The harmonics proceed as m*root.
+-- n and m are both arbitrary, so we're really redoing work here.
+-- We should just find the appropriate i for (r1/posn)*i.
+
+harmonizeRange :: Rational -> Rational -> [Rational] -> WS.T W Rational
+harmonizeRange lo hi [] = empty
+harmonizeRange lo hi (r1:rs) = 
+    wChoice . map pure . nub . catMaybes . map range . takeWhile (<= hi) $ [ s*n | n <- [1..] ]
+    where
+    ratios = map (/r1) rs
+    posn = fromIntegral (lcms (map denominator ratios))
+    s = r1/posn
+    range = listToMaybe . takeWhile (<= hi) . dropWhile (<= lo) . iterate (2*)
+
+withHarmonies :: Rational -> Rational -> [Rational] -> WS.T W [Rational]
+withHarmonies lo hi rs = do
+    r <- harmonizeRange lo hi rs
+    return . sort $ r : rs
 
 
 type D = Cs.D
@@ -100,3 +137,24 @@ playChord (a,b,c,d,e,f,g,h) = amp*o a + amp*o b + amp*o c + amp*o d + amp*o e + 
 
 playChords :: [[Rational]] -> Cs.Sig
 playChords rs = Cs.sched (return . playChord) (Cs.withDur 1 (Cs.cycleE (map chordSig rs) (Cs.metroE 1)))
+
+
+majorScale :: [Rational]
+majorScale = [1,9/8,5/4,4/3,3/2,10/6,15/8]
+
+minorScale :: [Rational]
+minorScale = [1,9/8,6/5,4/3,3/2,8/5,16/9]  -- 2-(5+5) 7th, 5+5 2nd
+minorScale' :: [Rational]
+minorScale' = [1,9/8,6/5,4/3,3/2,8/5,9/5]  -- m3+5 7th, 5+5 2nd
+minorScale'' :: [Rational]
+minorScale'' = [1,10/9,6/5,4/3,3/2,8/5,9/5] -- m3+5 7th, 4-m3 2nd
+minorScale''' :: [Rational]
+minorScale''' = [1,10/9,6/5,4/3,3/2,8/5,16/9] -- 2-(5+5) 7th, 4-m3 2nd
+
+parallel :: [Rational] -> [[Rational]]
+parallel scale = zipWith (\a b -> [a,b]) scale (drop 2 (scale ++ map (2*) scale))
+
+harmscale :: WS.T W [[Rational]]
+harmscale = do
+    mapM (withHarmonies (1/2) 1) (parallel minorScale)
+
